f0f6bcdb0dd671ca0c1b1e9bf70a7690ae47f251
Thrishank <86158204+thrishank@users.noreply.github.com>
2025-04-29 23:11:29 +0530

    Update solscan.ts

--- src/solscan.ts
+++ src/solscan.ts
@@ -2,7 +2,7 @@ const requestOptions = {
   headers: {
     method: "GET",
     token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE3MzM5MjYwODU1MTUsImVtYWlsIjoidGhyaXNoYW5ra2FsbHVydUBnbWFpbC5jb20iLCJhY3Rpb24iOiJ0b2tlbi1hcGkiLCJhcGlWZXJzaW9uIjoidjIiLCJpYXQiOjE3MzM5MjYwODV9.3NpAd_tGQ3LmRS3b7ZGQ5xnfNZXR3lSRUJqWhTWrsi8",
+      "",
   },
 };
 
